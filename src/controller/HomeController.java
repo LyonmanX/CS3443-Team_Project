6af26e6940dbd6d149004336beefdd4fe81a3a35
Ethan R <ethanren219@gmail.com>
2026-07-27 19:21:08 -0500

    Moved Data folder into a Resource folder which is inside of SRC.Created the HomeController along with the Home Screen FXML design

--- src/controller/HomeController.java
+++ src/controller/HomeController.java
@@ -1,4 +1,53 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * This class will be the controller for the Home Screen
+ */
+
 public class HomeController {
+
+    @FXML private Label totalGamesLabel;
+
+    @FXML private Label totalHoursLabel;
+
+    @FXML private Button gameLibraryButton;
+
+    /**
+     * initializes home screen
+     */
+
+    @FXML public void initialize() {
+
+        totalGamesLabel.setText("0");
+        totalHoursLabel.setText("0");
+
+    }
+
+    /**
+     * Opens Game Library
+     */
+
+    @FXML private void openGameLibrary(){
+
+    }
+
+    /**
+     *  Update home screen information
+     */
+
+    public void updateInformation(int totalGames, double totalHours) {
+
+        totalGamesLabel.setText(String.valueOf(totalGames));
+
+        if(totalHours == (int) totalHours) {
+            totalHoursLabel.setText(String.valueOf((int) totalHours));
+        }
+        else {
+            totalHoursLabel.setText(String.format("%.1f", totalHours));
+        }
+    }
 }
